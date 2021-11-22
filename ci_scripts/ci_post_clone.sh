5a34f361da52df698363403e9941f3498d25c880
Gio Lodi <giovanni.lodi42@gmail.com>
2021-11-22 16:25:33 +1100

    Add logs to `ci_post_clone.sh`
    Will they appear in real time?

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,5 +3,8 @@
 # The docs recommend using Homebrew for CocoaPods.
 # https://developer.apple.com/documentation/xcode/making-dependencies-available-to-xcode-cloud#Make-CocoaPods-Dependencies-Available-to-Xcode-Cloud
 
+echo "ℹ️: Install CocoaPods via Homebrew"
 brew install cocoapods
+
+echo "ℹ️: Install CocoaPods' libraries"
 pod install
