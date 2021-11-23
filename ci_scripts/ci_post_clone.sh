3c04c8cf3be69e0a9809be31823dd93aa7b70b6a
Gio Lodi <giovanni.lodi42@gmail.com>
2021-11-23 12:14:47 +1100

    Add logs for Ruby tooling

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,6 +3,21 @@
 # The docs recommend using Homebrew for CocoaPods.
 # https://developer.apple.com/documentation/xcode/making-dependencies-available-to-xcode-cloud#Make-CocoaPods-Dependencies-Available-to-Xcode-Cloud
 
+echo "ℹ️Ruby -v:"
+ruby -v
+
+echo "ℹ️rbenv?"
+which rbenv
+
+echo "ℹ️rvm?"
+which rvm
+
+echo "ℹ️chruby?"
+which chruby
+
+echo "👋Inspection Completed"
+exit 1
+
 echo "ℹ️: Install CocoaPods via Homebrew"
 brew install cocoapods
 
